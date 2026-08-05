b41eee77cf2a03a9d7523dc358b1804f21c7a4f3
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-06 00:56:07 +0200

    Change bot custom status to invite link
    Updated bot's custom status to a Discord invite link.

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "fucking niggers",     // this is what people actually see
+        state: "https://discord.gg/HGvtrSvK6w",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
