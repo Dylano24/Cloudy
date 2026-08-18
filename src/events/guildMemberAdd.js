88424525c1db154e3aff67404e69cae480806b31
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-18 21:03:18 +0200

    Use transparent C logo in welcome message

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -113,7 +113,7 @@ export default {
                                     }
                                 )
                                 .setThumbnail(
-                                    'https://cdn.jsdelivr.net/gh/Dylano24/Cloudy@e56cbead4b6d6ef364a56f421bab08f683d0965f/assets/cloudy-c-logo.png'
+                                    'https://cdn.jsdelivr.net/gh/Dylano24/Cloudy@main/assets/cloudy-c-logo.png'
                                 )
                                 .setImage(
                                     'https://cdn.jsdelivr.net/gh/Dylano24/Cloudy@b99c69899540531d92514df84f1c19a13a1350a0/assets/cloudy-dynamic-banner.gif'
