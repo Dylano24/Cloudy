bc74b553f4c503c42c86e23108d360fa7b785bc6
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-18 12:05:51 +0200

    Match Cloudy welcome footer to Renegade style

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -111,15 +111,18 @@ export default {
                                     user.displayAvatarURL({
                                         dynamic:true
                                     })
-                                )
-                                .setTimestamp();
+                                );
 
                             embed
                                 .setImage(
                                     'https://cdn.jsdelivr.net/gh/Dylano24/Cloudy@22049f372fe990ac257455957a045904461ec614/assets/cloudy-waves-welcome.gif'
                                 )
                                 .setFooter({
-                                    text: '© Cloudy • Build. Compete. Dominate.'
+                                    text: '© Cloudy • Build. Compete. Dominate.',
+                                    iconURL: member.client.user.displayAvatarURL({
+                                        extension: 'png',
+                                        size: 64
+                                    })
                                 });
 
 
