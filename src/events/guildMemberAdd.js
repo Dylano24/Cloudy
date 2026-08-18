ce8bf1de538d0f8ffafd0bc623fd38d6d1e2f516
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-18 13:54:18 +0200

    Add dynamic Cloudy welcome banner

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -111,7 +111,7 @@ export default {
                                     'https://cdn.jsdelivr.net/gh/Dylano24/Cloudy@a3523303898a32de406da2fea5864e1cce24bb11/assets/cloudy-logo.png'
                                 )
                                 .setImage(
-                                    'https://cdn.jsdelivr.net/gh/Dylano24/Cloudy@7f4f35a7cd9bf29bb2c8e1fd24d6ad2cca2677d9/assets/cloudy-welcome-banner.png'
+                                    'https://cdn.jsdelivr.net/gh/Dylano24/Cloudy@main/assets/cloudy-dynamic-banner.gif'
                                 );
 
                             // Force the footer into the final Discord API payload so it
