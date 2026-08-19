cae914f0d61b7612eda3bd330fb3a0db601b70da
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-19 10:12:39 +0200

    Use ticket UI override handlers

--- src/interactions/buttons/ticket/ticket.js
+++ src/interactions/buttons/ticket/ticket.js
@@ -1,20 +1,14 @@
 import createTicketHandler, {
   closeTicketHandler,
-  claimTicketHandler,
   priorityTicketHandler,
   pinTicketHandler,
-  unclaimTicketHandler,
-  reopenTicketHandler,
   deleteTicketHandler,
 } from '../../../handlers/ticketButtons.js';
 
 export default [
   createTicketHandler,
   closeTicketHandler,
-  claimTicketHandler,
   priorityTicketHandler,
   pinTicketHandler,
-  unclaimTicketHandler,
-  reopenTicketHandler,
   deleteTicketHandler,
 ];
