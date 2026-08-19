b85d400a2faef293a74e27a5169e086026363072
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-19 13:02:11 +0200

    Remove duplicate ticket panel create normalizer

--- src/events/ticketPanelMessageCreate.js
+++
@@ -1,9 +0,0 @@
-import { Events } from 'discord.js';
-import { normalizeTicketPanelMessage } from '../utils/ticket/ticketPanelAppearance.js';
-
-export default {
-  name: Events.MessageCreate,
-  async execute(message) {
-    await normalizeTicketPanelMessage(message);
-  },
-};
