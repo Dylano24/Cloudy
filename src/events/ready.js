6c33cb74c522d992204e0f74d67cb0d797e0c3d3
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-19 06:47:56 +0200

    Start reliable invite tracking monitor

--- src/events/ready.js
+++ src/events/ready.js
@@ -8,7 +8,7 @@ import { reconcileLevelRoles } from "../services/leveling/levelRoleSyncService.j
 import { initRiffyAfterReady } from "../services/music/riffySetup.js";
 import { startRustPatchNotes } from "../services/rustPatchNotesService.js";
 import { scanProtectedIdentities } from "../services/protectedIdentityService.js";
-import { initializeInviteTracking } from "../services/inviteTrackingService.js";
+import { startInviteTracking } from "../services/inviteTrackingService.js";
 
 export default {
   name: Events.ClientReady,
@@ -46,7 +46,7 @@ export default {
 
       startRustPatchNotes(client);
       void scanProtectedIdentities(client);
-      await initializeInviteTracking(client);
+      await startInviteTracking(client);
 
       if (client.config?.features?.music) {
         initRiffyAfterReady(client);
