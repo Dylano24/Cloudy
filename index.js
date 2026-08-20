02c451f91835b210e20da6ca048539713a00887d
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-20 13:20:50 +0200

    Prevent duplicate Railway bot instances

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import './src/app.js';
+const PRIMARY_RAILWAY_PROJECT_ID = '0dae5cd5-d555-4150-bce8-44fca806bfb3';
+const railwayProjectId = process.env.RAILWAY_PROJECT_ID || null;
+
+if (railwayProjectId && railwayProjectId !== PRIMARY_RAILWAY_PROJECT_ID) {
+  console.error(
+    `[startup] Refusing to start Discord client from secondary Railway project ${railwayProjectId}. ` +
+    `Primary Cloudy project is ${PRIMARY_RAILWAY_PROJECT_ID}.`
+  );
+  process.exit(0);
+}
+
+await import('./src/app.js');
