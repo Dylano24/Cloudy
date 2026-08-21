c2deb193251509862e43994f4152598f6970e049
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-21 14:37:40 +0200

    Fix ticket created channel link

--- src/interactions/modals/ticket/zzTicketReliabilityModals.js
+++ src/interactions/modals/ticket/zzTicketReliabilityModals.js
@@ -1,4 +1,4 @@
-import { MessageFlags } from 'discord.js';
+import { MessageFlags, PermissionFlagsBits } from 'discord.js';
 import { successEmbed } from '../../../utils/embeds.js';
 import { InteractionHelper } from '../../../utils/interactionHelper.js';
 import { handleInteractionError } from '../../../utils/errorHandler.js';
@@ -9,6 +9,47 @@ import {
   createTicket,
 } from '../../../services/ticketReliabilityService.js';
 import { logger } from '../../../utils/logger.js';
+
+async function ensureTicketCreatorAccess(channel, userId) {
+  const requiredPermissions = [
+    PermissionFlagsBits.ViewChannel,
+    PermissionFlagsBits.SendMessages,
+    PermissionFlagsBits.ReadMessageHistory,
+    PermissionFlagsBits.AttachFiles,
+  ];
+
+  const member = await channel.guild.members.fetch(userId).catch(() => null);
+  const permissions = member ? channel.permissionsFor(member) : null;
+
+  if (permissions?.has(requiredPermissions)) {
+    return true;
+  }
+
+  try {
+    await channel.permissionOverwrites.edit(userId, {
+      ViewChannel: true,
+      SendMessages: true,
+      ReadMessageHistory: true,
+      AttachFiles: true,
+    }, {
+      reason: 'Ensure ticket creator can access newly created ticket',
+    });
+    return true;
+  } catch (error) {
+    logger.warn('Could not repair ticket creator permissions', {
+      guildId: channel.guild.id,
+      channelId: channel.id,
+      userId,
+      error: error.message,
+    });
+    return false;
+  }
+}
+
+function buildTicketChannelLink(channel) {
+  const safeName = String(channel?.name || 'ticket').replace(/([\\[\\]])/g, '\\$1');
+  return `[#${safeName}](https://discord.com/channels/${channel.guild.id}/${channel.id})`;
+}
 
 const createTicketModal = {
   name: 'create_ticket_modal',
@@ -31,8 +72,16 @@ const createTicketModal = {
         reason,
       );
 
+      const hasAccess = await ensureTicketCreatorAccess(channel, interaction.user.id);
+      if (!hasAccess) {
+        throw Object.assign(new Error('Ticket creator access could not be verified'), {
+          userMessage: 'Your ticket was created, but Cloudy could not verify your access to it. Please contact an admin.',
+        });
+      }
+
+      const channelLink = buildTicketChannelLink(channel);
       await InteractionHelper.safeEditReply(interaction, {
-        embeds: [successEmbed('Ticket Created', `Your ticket has been created in ${channel}!`)],
+        embeds: [successEmbed('Ticket Created', `Your ticket has been created in ${channelLink}!`)],
       });
     } catch (error) {
       if (error?.userMessage && (interaction.deferred || interaction.replied)) {
