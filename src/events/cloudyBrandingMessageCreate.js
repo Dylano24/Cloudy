f3251161c1929a9fd563ac2afc2a50f5a4ea1e0f
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-23 22:43:18 +0200

    Preserve community review embeds

--- src/events/cloudyBrandingMessageCreate.js
+++ src/events/cloudyBrandingMessageCreate.js
@@ -1,6 +1,8 @@
 import { Events } from 'discord.js';
 import { normalizeCloudyMessage } from '../services/cloudyBrandingService.js';
 
+const COMMUNITY_REVIEWS_CHANNEL_ID = '1540625438601379961';
+
 export default {
   name: Events.MessageCreate,
   once: false,
@@ -9,6 +11,7 @@ export default {
     if (!message?.client?.user?.id) return;
     if (message.author?.id !== message.client.user.id) return;
     if (!message.embeds?.length) return;
+    if (message.channelId === COMMUNITY_REVIEWS_CHANNEL_ID) return;
 
     const timer = setTimeout(() => {
       void normalizeCloudyMessage(message, { ensureFooter: true });
