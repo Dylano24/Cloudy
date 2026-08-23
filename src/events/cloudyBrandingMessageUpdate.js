0caf23fdc8cd865973c8c3e358e3b68da9a241a5
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-23 22:46:40 +0200

    Restore Cloudy branding updates for community reviews

--- src/events/cloudyBrandingMessageUpdate.js
+++ src/events/cloudyBrandingMessageUpdate.js
@@ -1,8 +1,6 @@
 import { Events } from 'discord.js';
 import { normalizeCloudyMessage } from '../services/cloudyBrandingService.js';
 
-const COMMUNITY_REVIEWS_CHANNEL_ID = '1540625438601379961';
-
 export default {
   name: Events.MessageUpdate,
   once: false,
@@ -17,7 +15,6 @@ export default {
     if (!message) return;
     if (message.author?.id !== message.client.user.id) return;
     if (!message.embeds?.length) return;
-    if (message.channelId === COMMUNITY_REVIEWS_CHANNEL_ID) return;
 
     const timer = setTimeout(() => {
       void normalizeCloudyMessage(message, { ensureFooter: true });
