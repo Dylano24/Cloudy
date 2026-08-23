ed54dc120dcfc4973f33579f5bf3ad30506b5cf7
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-23 14:05:04 +0200

    Update staff review interaction test state

--- test/staffReviewsInteraction.test.js
+++ test/staffReviewsInteraction.test.js
@@ -5,12 +5,14 @@ import staffReviewsInteraction from '../src/events/staffReviewsInteraction.js';
 import {
   STAFF_REVIEW_MODAL_ID,
   rememberRating,
+  rememberReviewMember,
 } from '../src/services/staffReviewsService.js';
 
 test('staff review is not published when deferReply rejects', async () => {
   let channelFetches = 0;
   const user = { id: 'defer-failure-user' };
 
+  rememberReviewMember(user.id, 'owner-member-id');
   rememberRating(user.id, 5);
 
   const interaction = {
