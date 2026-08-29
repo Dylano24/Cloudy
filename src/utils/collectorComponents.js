b87f1d0db42c12283691ec62f05a047ddcc9df9a
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-29 19:28:05 +0200

    Route embed manager channel and embed controls to collectors

--- src/utils/collectorComponents.js
+++ src/utils/collectorComponents.js
@@ -9,6 +9,11 @@ const COLLECTOR_MANAGED_PREFIXES = [
   'simple_embed_owner_emoji',
   'simple_embed_modify_select',
   'simple_embed_modify_page',
+  'simple_embed_modify_channel',
+  'simple_embed_modify_channel_page',
+  'simple_embed_modify_embed',
+  'simple_embed_modify_embed_page',
+  'simple_embed_modify_back',
 ];
 
 export function isCollectorManagedComponent(customId = '') {
