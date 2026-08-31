f553b019df0cc4d2d155cac39866d8ea7e179f9e
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-08-31 13:06:19 +0200

    feat: add casino games and economy guide

---
+++ src/commands/Economy/gambling.js
@@ -0,0 +1,13 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { infoEmbed } from '../../utils/embeds.js';
+import { buildGamblingCommandListText } from '../../config/gamblingCommands.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+
+export default {
+  data: new SlashCommandBuilder().setName('gambling').setDescription('Gambling and economy information')
+    .addSubcommand(subcommand => subcommand.setName('info').setDescription('Show all gambling and economy commands')),
+  category: 'Economy',
+  async execute(interaction) {
+    await InteractionHelper.safeReply(interaction, { embeds: [infoEmbed('Gambling', buildGamblingCommandListText())], components: [] });
+  },
+};
