b5a0ea931652c18a76b0b2dd121181193d71ef6d
Dylano24 <Dylanozoetermeer079@gmail.com>
2026-09-10 03:19:25 +0200

    Delete short-lived bot status messages

--- src/events/dedicatedChannelsMessageCreate.js
+++ src/events/dedicatedChannelsMessageCreate.js
@@ -1,9 +1,14 @@
 import { Events } from 'discord.js';
 import { scheduleDedicatedChannelGuide } from '../services/dedicatedChannelService.js';
+import { scheduleTransientMessageDeletion } from '../utils/transientResponse.js';
 
 export default {
   name: Events.MessageCreate,
   execute(message) {
     scheduleDedicatedChannelGuide(message);
+
+    if (message.author?.id === message.client?.user?.id) {
+      scheduleTransientMessageDeletion(message);
+    }
   },
 };
